8c1c5f2d8b0ef378186623e7f94d537cbb468aaf
Archit Kapoor <archit.imsec10@gmail.com>
2019-06-22 20:20:53 +0530

    Adding a few constants, related to game, like the target number of runs to chase, and the maximum overs to left to chase down the target

---
+++ app/constants.py
@@ -0,0 +1,10 @@
+# The possibilities of runs that can be scored in each ball,
+# or the batsmen can be knocked out
+SCORE_KEYS = ['0', '1', '2', '3', '4', '5', '6', 'OUT']
+
+# Number of runs to chase
+TARGET_RUNS = 40
+
+# Maximum number of overs left to chase
+MAX_OVERS = 4
+
